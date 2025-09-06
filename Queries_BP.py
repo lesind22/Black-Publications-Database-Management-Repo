e535aec905f4512ecb7bf36894f30f94b24f5f07
Indira Lessington <ilessin@g.clemson.edu>
2025-09-06 17:34:03 -0400

    changes to historical question

--- Queries_BP.py
+++ Queries_BP.py
@@ -1,12 +1,12 @@
 
 
-Simple Analysis of Black Publications Dataset 18th-21st centuries
-Century 
+# Simple Analysis of Black Publications Dataset 18th-21st centuries
+# Century 
 
-import sqlite3
-import pandas as pd 
 
-The question I want to ask from what I've been working with
-so far is "What city and state did these publications thrive
-in from the years 1869-1969". "
-It's a working question that is subject to change but that's where my thinking is right now
+# The question I want to ask from what I've been working with
+# so far is "What city and state did these publications thrive
+# in from the years 1869-1969". I chose those years because historically 
+# what was happening throughout the US and abroad. 
+# It's a working question that is subject to change but that's where my thinking is right now
+
