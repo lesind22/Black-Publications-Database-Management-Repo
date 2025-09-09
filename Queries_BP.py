f7b4d817462fdc5b50a846763ee731f08b179906
Indira Lessington <ilessin@g.clemson.edu>
2025-09-09 17:31:05 -0400

    historical ? addition

--- Queries_BP.py
+++ Queries_BP.py
@@ -8,5 +8,11 @@
 # so far is "What city and state did these publications thrive
 # in from the years 1869-1969". I chose those years because historically 
 # what was happening throughout the US and abroad. 
+# "What publishing company did the most publishing between 1900-1919"
 # It's a working question that is subject to change but that's where my thinking is right now
+
+import sqlite 3
+import pandas as pd
+
+
 
