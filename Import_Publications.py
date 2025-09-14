02d81a64f0402a5d6b911220e20877d089e3c059
Indira Lessington <ilessin@g.clemson.edu>
2025-09-14 17:52:59 -0400

    changes to .py

--- Import_Publications.py
+++ Import_Publications.py
@@ -28,22 +28,27 @@ with open('Ad_Type_BP.csv', 'r') as csv_file:
 
     # Prepare data for insertion
     data = [
-        ("The Anglo-African Magazine", "Sponsored Content", "2", "3"),
-        ("Ringwood's Afro American Journal of Fashion", "Sponsored Content", "2", "5"),
-        ("The Voice of the Negro", "Public Service Announcement", "4", "46"),
-        ("The Crisis", "Sponsored Content", "68", "13,000"),
-        ("The Negro Yearbook: An Encyclopedia of the Negro", "Sponsored Content", "1", "1"),
-        ("The Negro Yearbook: An Encyclopedia of the Negro", "Sponsored Content", "1", "2"),
-        ("The Half-Century Magazine", "Sponsored Content", "18", "77"),
-        ("Black History Bulletin", "Sponsored Content", "88", "176"),
-        ("The Black Panther Newspaper", "Public Service Announcement", "15", "30"),
-        ("Black World (Negro Digest)", "Public Service Announcement", "25", "53"),
-        ("Ebony", "Sponsored Content", "80", "60"),
-        ("Jet", "Sponsored Content", "60", "31,000"),
-        ("Essence", "Sponsored Content", "55", "440"),
-        ("Ebony Jr!", "Public Service Announcement", "7", "71"),
-        ("Muhammad Speaks", "Public Service Announcement", "15", "780")
-    ]
+    ("Ringwood's Afro American Journal of Fashion", "Sponsored Content", "2", "5"),
+    ("The Voice of the Negro", "Public Service Announcement", "4", "46"),
+    ("The Crisis", "Sponsored Content", "68", "13,000"),
+    ("The Negro Yearbook: An Encyclopedia of the Negro", "Sponsored Content", "1", "1"),
+    ("The Negro Yearbook: An Encyclopedia of the Negro", "Sponsored Content", "1", "2"),
+    ("The Half-Century Magazine", "Sponsored Content", "18", "77"),
+    ("Black History Bulletin", "Sponsored Content", "88", "176"),
+    ("The Black Panther Newspaper", "Public Service Announcement", "15", "30"),
+    ("Black World (Negro Digest)", "Public Service Announcement", "25", "53"),
+    ("Ebony", "Sponsored Content", "80", "60"),
+    ("Jet", "Sponsored Content", "60", "31,000"),
+    ("Essence", "Sponsored Content", "55", "440"),
+    ("Ebony Jr!", "Public Service Announcement", "7", "71"),
+    ("Muhammad Speaks", "Public Service Announcement", "15", "780"),
+    ("The BAD Times (Black Americans for Democracy)", "Sponsored Content", "20", ""), 
+    ("Black Enterprise", "Sponsored Content", "40", "480"),
+    ("Black Dialogue", "Sponsored Content", "4", "5"),
+    ("The Grassroot Struggle: The Official Organ of the Black Citizens Task Force", "Sponsored Content", "2", "12"),  
+    ("Freedomways", "Sponsored Content", "24", "28"),  
+    ("Soulbook: Quarterly Journal of Revolutionary Afroamerica", "Sponsored Content", "3", "8"),  
+]
 
 # Insert data into the `publications` table
 cursor.executemany('''
