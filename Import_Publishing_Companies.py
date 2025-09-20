ec41844528ae3bef807b030b0d9aef1a0ae8d85f
Indira Lessington <ilessin@g.clemson.edu>
2025-09-20 17:06:32 -0400

    changes to script

--- Import_Publishing_Companies.py
+++ Import_Publishing_Companies.py
@@ -22,14 +22,23 @@ print("Table `publishing_companies` created successfully!")
 
 # Sample data for publishing companies
 data = [
-    ("Johnson Publishing Company", "Chicago", "Illinois", "1942", "Publisher of Ebony and Jet magazines."),
-    ("The Crisis Publishing Company", "New York City", "New York", 1910, "Publisher of The Crisis magazine."),
-    ("Freedomways Associates", "New York City", "New York", 1961, "Publisher of Freedomways journal."),
-    ("The Black Panther Party", "Oakland", "California", 1966, "Publisher of The Black Panther Newspaper."),
-    ("The Half-Century Magazine Company", "Chicago", "Illinois", 1916, "Publisher of The Half-Century Magazine."),
-    ("Essence Communications Inc.", "New York City", "New York", 1968, "Publisher of Essence magazine."),
-    ("Black Enterprise Publishing", "New York City", "New York", 1970, "Publisher of Black Enterprise magazine."),
-    ("Thomas and Robert Hamiltion", "New York City", "New York", 1858-1860, "Publishers of The Anglo African magazine"), 
+    ("Samuel Cornish and John Russwurm", "New York City", "New York", "1827", "Publishers of Freedom's Journal"),
+    ("Frederick Douglass", "Rochester", "New York", "1847", "Publisher of The North Star Newspaper"),
+    ("Mary Ann Shadd Cary", "Windor, Toronto, and Chatham", "Canada", "1853", "Publisher of The Provincial Freeman Newspaper"),
+    ("African Methodist Episcopal Church", "Philadelphia", "Pennsylvania", "1854" "Publisher of The Christian Recorder Newspaper"),
+    ("Sears", "Beaufort", "South Carolina", "1862", "Publisher of The New South Newspaper"),
+    ("Publisher of The California Eagle Newspaper"),
+    ("The Publisher of The Woman's Era Newspaper"),
+    ("Opportunity: A Journal of Negro Life"),
+    ("Johnson Publishing Company", "Chicago", "Illinois", "1942", "Publisher of Ebony and Jet magazines"),
+    ("The Crisis Publishing Company", "New York City", "New York", "1910", "Publisher of The Crisis magazine"),
+    ("Freedomways Associates", "New York City", "New York", "1961", "Publisher of Freedomways journal"),
+    ("The Black Panther Party", "Oakland", "California", "1966", "Publisher of The Black Panther Newspaper"),
+    ("The Half-Century Magazine Company", "Chicago", "Illinois", "1916", "Publisher of The Half-Century Magazine"),
+    ("Essence Communications Inc.", "New York City", "New York", "1968", "Publisher of Essence magazine"),
+    ("Black Enterprise Publishing", "New York City", "New York", "1970", "Publisher of Black Enterprise magazine"),
+    ("J.L. Nichols Publishing Company", "New York City", "New York", "1858-1860", "Publishers of The Anglo African magazine"), 
+    ("Publisher of The Root Magazine")
     
 ]
 
